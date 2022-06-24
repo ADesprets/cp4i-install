820f8fe4ad3ffc62c28899ef8c54bea80704d8b7
GMX\060948706 <arnauld_desprets@fr.ibm.com>
2022-06-24 11:06:38 +0200

    Merge capability

--- lib.sh
+++ lib.sh
@@ -1,9 +1,32 @@
-# asset that variablke is defined
+read_config_file(){
+	if test -n "$PC_CONFIG";then
+	  config_file="$PC_CONFIG"
+	else
+	  config_file="$1"
+	fi
+	if test -z "$config_file";then
+		mylog error "Usage: $0 <config file>" 1>&2
+		mylog info "Example: $0 ${scriptdir}cp4i.conf"
+		exit 1
+	fi
+
+	if test ! -e "${config_file}";then
+		mylog error "No such file: $config_file" 1>&2
+		exit 1
+	fi
+
+	# load user specific variables, "set -a" so that variables are part of environment for envsubst
+	set -a
+	. "${config_file}"
+	set +a
+}
+
+# assert that variable is defined
 # @param 1 name of variable
 # @param 2 error message, or name of method to call if begins with "fix"
 var_fail(){
 	if eval test -z '$'$1;then
-		mylog error "ERROR: missing config variable: $1" 1>&2
+		mylog error "missing config variable: $1" 1>&2
 		case "$2" in
 			fix*|echo*) eval $2 ;;
 			"") ;;
@@ -14,7 +37,7 @@ var_fail(){
 }
 
 # simple logging with colors
-# first param: level (info/warn/error/wait)
+# @param 1 level (info/error/warn/wait/check/ok/no)
 mylog(){
 	p=
 	w=
@@ -31,6 +54,49 @@ mylog(){
 	esac
 	shift
 	echo $w "$(tput setaf $c)$p$@$s$(tput setaf 9)";
+}
+
+################################################
+# Log in IBM Cloud
+Login2IBMCloud () {
+  var_fail my_ic_apikey "Create and save API key JSON file from: https://cloud.ibm.com/iam/apikeys"
+  mylog check "Login to IBM Cloud"
+  if ! ibmcloud login -q --no-region --apikey $my_ic_apikey > /dev/null;then
+    mylog error "Fail to login to IBM Cloud, check API key: $my_ic_apikey" 1>&2
+    exit 1
+  else mylog ok
+  fi
+}
+
+# wait for Cluster availability
+# set variable my_cluster_url
+Wait4ClusterAvailability () {
+  wait_for_state 'Cluster state' 'normal-All Workers Normal' "ibmcloud oc cluster get --cluster $my_ic_cluster_name --output json|jq -r '.state+\"-\"+.status'"
+
+  mylog check "Checking Cluster URL"
+  my_cluster_url=$(ibmcloud ks cluster get --cluster $my_ic_cluster_name --output json | jq -r .serverURL)
+  case "$my_cluster_url" in
+	https://*)
+	mylog ok " -> $my_cluster_url"
+	;;
+	*)
+	mylog error "Error getting cluster URL for $my_ic_cluster_name" 1>&2
+	exit 1
+	;;
+  esac
+}
+
+################################################
+# Login to openshift cluster
+# note that this login requires that you login to the cluster once (using sso or web): not sure why
+# requires var my_cluster_url
+Login2OpenshiftCluster () {
+  mylog check "Login to cluster"
+  while ! oc login -u apikey -p $my_ic_apikey --server=$my_cluster_url > /dev/null;do
+	mylog error "$(date) Fail to login to Cluster, retry in a while (login using web to unblock)" 1>&2
+	sleep 30
+  done
+  mylog ok
 }
 
 # wait for command to return specified value
@@ -79,6 +145,21 @@ check_create_oc_yaml(){
 	mylog check "Checking ${octype} ${name}"
 	if oc get ${octype} ${name} -n ${ns} > /dev/null 2>&1; then mylog ok;else
 		envsubst < "${yaml}" | oc apply -f - || exit 1
+	fi
+}
+
+check_create_oc_openldap(){
+	local octype="$1"
+	local name="$2"
+	mylog check "Checking ${octype} ${name}"
+	if oc get ${octype} ${name} > /dev/null 2>&1; then mylog ok;else
+      oc new-app openshift/${name}
+      oc expose service/${name}
+      oc get service ${name} -o json  | jq '.spec.ports[0] += {"Nodeport":30389}' | jq '.spec.ports[1] += {"Nodeport":30686}' | jq '.spec.type |= "NodePort"' | oc apply -f -
+      port=`oc get service ${name} -o json  | jq -r '.spec.ports[0].nodePort'`
+      hostname=`oc get route ${name} -o json | jq -r '.spec.host'`
+      envsubst < "${ldapdir}Import.tmpl" > "${ldapdir}Import.ldiff"
+      ldapmodify -H ldap://$hostname:$port -D "$my_dn_openldap" -w admin -f ${ldapdir}Import.ldiff
 	fi
 }
 
