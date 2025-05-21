991e6f15d3652082160a727f15d4d63ea3d78056
GMX\060948706 <arnauld_desprets@fr.ibm.com>
2025-05-21 17:35:00 +0200

    add mq_simple erased by mistake

---
+++ demos/mq_simple/scripts/mqapp_tmpl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#export MQCCDTURL=$MQCCDTURL
+#export MQSSLKEYR=$MQSSLKEYR
+#amqs${MQ_APP_LETTER}hac $QUEUE_TMPL $QMGR_TMPL
+
+export MQCCDTURL=${MY_MQ_WORKINGDIR}cp4iadmq/json/ccdt.json
+export MQSSLKEYR=${MY_MQ_WORKINGDIR}cp4iadmq/tls/clnt1/clnt1-keystore.kdb
+amqsphac Q1 CP4IADMQ
