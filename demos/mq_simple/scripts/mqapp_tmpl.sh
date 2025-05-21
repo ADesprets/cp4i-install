b2f31f600eea60a13776a90fbf538ac33a48ef59
GMX\060948706 <arnauld_desprets@fr.ibm.com>
2025-05-21 11:10:35 +0200

    remove one demo un-needed

--- demos/mq_simple/scripts/mqapp_tmpl.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#export MQCCDTURL=$MQCCDTURL
-#export MQSSLKEYR=$MQSSLKEYR
-#amqs${MQ_APP_LETTER}hac $QUEUE_TMPL $QMGR_TMPL
-
-export MQCCDTURL=${MY_MQ_WORKINGDIR}cp4iadmq/json/ccdt.json
-export MQSSLKEYR=${MY_MQ_WORKINGDIR}cp4iadmq/tls/clnt1/clnt1-keystore.kdb
-amqsphac Q1 CP4IADMQ
