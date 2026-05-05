cd0cd1508d628ea0283ddb824027e34610f5e1f5
Arnauld Desprets <arnauld_desprets@fr.ibm.com>
2026-05-05 10:58:17 +0200

    Fix KConnect for new version

--- demos/es_simple/scripts/es.config.sh
+++ demos/es_simple/scripts/es.config.sh
@@ -127,14 +127,13 @@ function es_run_all () {
   check_directory_exist_create "${MY_ES_WORKINGDIR}"
 
   # Create namespace 
-  #toto
-  # create_project "$VAR_ES_NAMESPACE" "$VAR_ES_NAMESPACE project" "For Event Streams customisation" "${MY_RESOURCESDIR}" "${MY_ES_WORKINGDIR}"
+  create_project "$VAR_ES_NAMESPACE" "$VAR_ES_NAMESPACE project" "For Event Streams customisation" "${MY_RESOURCESDIR}" "${MY_ES_WORKINGDIR}"
 
-  # create_es
+  create_es
 
-  # create_kafka_topics
+  create_kafka_topics
 
-  # create_kafka_users
+  create_kafka_users
 
   create_kafka_connector
 
